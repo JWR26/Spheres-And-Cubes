2812edf1dfd4c47cff7a4d27077427751a495b2a
Jonathan WEBSTER <64645223+JWR26@users.noreply.github.com>
2023-03-11 22:11:32 +0000

    initial commit
    empty commit to initialise the folder structure

---
+++ entities/Piece3D.gd
@@ -0,0 +1,14 @@
+class_name Piece3D
+""" Class to represent the pieces of the game"""
+
+extends Node3D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
